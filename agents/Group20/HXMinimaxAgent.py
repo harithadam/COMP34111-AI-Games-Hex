eb6dbd51bbed2573eedb37720d41e9fe5a68ecae
hk414 <hui.koh-10@student.manchester.ac.uk>
2025-11-23 13:20:18 +0000

    minimax with alpha beta pruning

--- agents/Group20/HXMinimaxAgent.py
+++ agents/Group20/HXMinimaxAgent.py
@@ -7,37 +7,34 @@ import math
 import random
 import copy
 
-
 class HXMinimaxAgent(AgentBase):
 
     def __init__(self, colour: Colour):
         super().__init__(colour)
-        self.max_depth = 2  # increase if time allows
+        self.max_depth = 2 
 
     def make_move(self, turn: int, board: Board, opp_move: Move | None) -> Move:
         legal_moves = self.get_legal_moves(board)
-        
+
         # Handle swap rule on turn 2
         if turn == 2 and not self.has_swapped(board):
-            legal_moves.append(Move(-1, -1))  # Add swap move
+            legal_moves.append(Move(-1, -1))  # Swap move
 
-        # Shuffle moves to diversify play
-        random.shuffle(legal_moves)
+        random.shuffle(legal_moves)  # diversify play
 
         best_score = -math.inf
-        best_move = legal_moves[0]  # default to first move
+        best_move = legal_moves[0]  # default
         alpha, beta = -math.inf, math.inf
 
         for move in legal_moves:
+
             if move.is_swap():
-                # For swap move, simulate the board state after swapping
                 score = self.evaluate_swap_position(board)
             else:
-                # simulate move
+                
                 board.set_tile_colour(move.x, move.y, self.colour)
                 score = self.minimax(board, depth=self.max_depth - 1,
-                                   alpha=alpha, beta=beta, maximizing=False)
-                # undo move
+                                     alpha=alpha, beta=beta, maximizing=False)
                 board.set_tile_colour(move.x, move.y, None)
 
             if score > best_score:
@@ -46,20 +43,58 @@ class HXMinimaxAgent(AgentBase):
 
             alpha = max(alpha, score)
 
-            if beta <= alpha:
-                break
-
         return best_move
-    
-    def evaluate_swap_position(self, board: Board) -> float:
-        """Evaluate the position after a potential swap"""
-        # My heuristic: swap if opponent's first move seems strong
-        # Give a score based on opponent's position to see whether its an advantageous pos
-        opp_score = 0
+
+    def minimax(self, board: Board, depth: int, alpha, beta, maximizing: bool):
+        if board.has_ended(self.colour):
+            return 10000 + depth
+        if board.has_ended(self.opp_colour()):
+            return -10000 - depth
+        if depth == 0:
+            return self.evaluate(board)
+
+        legal_moves = self.get_legal_moves(board)
+
+        if maximizing:
+            value = -math.inf
+            for move in legal_moves:
+                board.set_tile_colour(move.x, move.y, self.colour)
+                value = max(value, self.minimax(board, depth - 1, alpha, beta, False))
+                board.set_tile_colour(move.x, move.y, None)
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    break
+            return value
+        else:
+            value = math.inf
+            for move in legal_moves:
+                board.set_tile_colour(move.x, move.y, self.opp_colour())
+                value = min(value, self.minimax(board, depth - 1, alpha, beta, True))
+                board.set_tile_colour(move.x, move.y, None)
+                beta = min(beta, value)
+                if alpha >= beta:
+                    break
+            return value
+
+    def get_legal_moves(self, board: Board):
+        legal_moves = []
         for i in range(board.size):
             for j in range(board.size):
-                if board.tiles[i][j].colour == self.opp_colour():
-                    # Reward center control for opponent
-                    opp_score += 5 - abs(i - board.size/2) - abs(j - board.size/2)
-        return opp_score  
+                if board.tiles[i][j].colour is None:
+                    legal_moves.append(Move(i, j))
+        return legal_moves
 
+    def has_swapped(self, board: Board) -> bool:
+        for i in range(board.size):
+            for j in range(board.size):
+                if board.tiles[i][j].colour is not None:
+                    return True
+        return False
+
+    def evaluate(self, board: Board) -> int:
+        pass
+
+    def evaluate_swap_position(self, board: Board) -> int:
+        pass
+
+    
